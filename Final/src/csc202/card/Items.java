120d43b47f0794421ad2db15ed29350f56d72280
indica lit <jansenjustin.jj@gmail.com>
2011-12-01 00:34:53 -0500

    Added some stuff but did not finish because nathan wanted to do some stuff so... i pushed now so he can at least have taht

---
+++ Final/src/csc202/card/Items.java
@@ -0,0 +1,86 @@
+package csc202.card;
+/**
+ * Class used for Order.java Designed to hold the items within the order. 
+ * @author Justin Jansen
+ * Date: 11/30/11
+ */
+public class Items {
+	private String item;
+	private double cost;
+	private int quantity;
+	/**
+	 * For Adding just one of an item
+	 * @param item - The name of the item
+	 * @param cost - The price of the item
+	 */
+	public Items(String item, double cost){
+		this.item = item;
+		this.cost = cost;
+		quantity = 1;
+	}
+	/**
+	 * For Adding more than one of a certain item
+	 * @param item - The name of the item
+	 * @param cost - The Price of the item
+	 * @param quantity - The Number of items
+	 */
+	public Items(String item, double cost, int quantity) throws Exception{
+		if(quantity<=0){
+			throw new Exception("No Negitives");
+		}
+		else{
+			this.item = item;
+			this.cost = cost;
+			this.quantity = quantity;
+		}
+	}
+	/**
+	 * Set Item Name
+	 * @param s - Item Name
+	 */
+	public void setItem(String s){
+		item = s;
+	}
+	/**
+	 * Get item name
+	 * @return returns the name of the item
+	 */
+	public String getItem(){
+		return item;
+	}
+	/**
+	 * Set price of the Item
+	 * @param d - The price of the item
+	 */
+	public void setCost(double d){
+		cost = d;
+	}
+	/**
+	 * Get the price of the item
+	 * @return - The price of the item
+	 */
+	public double getCost(){
+		return cost;
+	}
+	/**
+	 * Set the number of items
+	 * @param i - Set the number of items
+	 */
+	public void setQuantity(int i){
+		quantity = i;
+	}
+	/**
+	 * Get the quantity of this item
+	 * @return - The quantity of this item
+	 */
+	public int getQuanity(){
+		return quantity;
+	}
+	/**
+	 * Get the combined price 
+	 * @return - The Combined price 
+	 */
+	public double getTotalPrice(){
+		return cost * (double)quantity;
+	}
+}
---
+++ Final/src/csc202/card/Order.java
@@ -0,0 +1,103 @@
+package csc202.card;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.LinkedList;
+
+/**
+ * This will contain the order information.
+ * @author Justin Jansen
+ * Date: 11/30/11
+ */
+public class Order implements Comparable<Order>{
+	private String orderNumber = "";
+	private Calendar date = Calendar.getInstance();
+	private SimpleDateFormat dateForm = new SimpleDateFormat("'Date:' yyyy.MM.dd '\nTime:' hh:mm:ss a zzz");
+	private LinkedList<Items> items = new LinkedList<Items>();
+	/**
+	 * Order Number
+	 * @param number - Order Number
+	 */
+	public void setNumber(String number){
+		orderNumber = number;
+		dateForm.format(date.getTime());
+	}
+	/**
+	 * @return Order Number
+	 */
+	public String getNumber(){
+		return orderNumber;
+	}
+	/**
+	 * Adding and item with quantity of one
+	 * @param item - Name
+	 * @param cost - Price
+	 */
+	// TODO Check list for Item If Item is Already On list Update Quantity
+	public void addItem(String item, double cost){
+		Items i = new Items(item,cost);
+		items.add(i);
+	}
+	/**
+	 * Adding Item with quantity more than one
+	 * @param item - Name 
+	 * @param cost - Price
+	 * @param quantity - Number of Items
+	 * @throws Exception - if quantity is negative
+	 */
+	
+	// TODO Check list for Item If Item is Already On list Update Quantity
+	public void addItem(String item, double cost, int quantity) throws Exception{
+		Items i = new Items(item, cost, quantity);
+		items.add(i);
+	}
+	/**
+	 * 
+	 * @param item - Item Name
+	 * @throws Exception - 
+	 */
+	public void removeItem(String item) throws Exception{
+		// TODO write this
+		if(true){
+			//remove
+		}
+		else{
+			throw new Exception("Item Not Found");
+		}
+	}
+	/**
+	 * Number of Items
+	 * @return Returns the number of times, not including the quantity of each type of item
+	 */
+	public int numberOfItems(){
+		return items.size();
+	}
+	/**
+	 * @return the total Number including quantity
+	 */
+	public int totalQuantityOfItems(){
+		int total = 0;
+		for(int i = 0;items.size()>i;i++){
+			total =+ items.get(i).getQuanity();
+		}
+		return total;
+	}
+	/**
+	 * Sets The Time and Date to Current Time and Date
+	 */
+	public void resetDate(){
+		dateForm.format(date.getTime());
+	}
+	/**
+	 * @return The Entire Linked List of Items
+	 */
+	public LinkedList<Items> getItemsList(){
+		return items;
+	}
+	
+	@Override
+	public int compareTo(Order arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
